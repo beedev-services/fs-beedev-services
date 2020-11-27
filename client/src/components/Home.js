06cd7a1d2425958b046df61c18ddb71a327e3ea2
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-11-27 17:30:36 -0500

    added gif overlay

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,14 +5,16 @@ const Home = () => {
         <>
         <h1>Let our Hive make a difference!!</h1>
         <div className='home-imgs'>
-            <img src="https://images.craftsnherbs.com/beedev/webdev.png" usemap="#image-map" id='heading' alt='' />
-
-            <map name="image-map">
+            <div className='webdev'></div>
+            <div className='overlay' ></div>
+            {/* <img src="https://images.craftsnherbs.com/beedev/webdev.png" usemap="#image-map" id='heading' alt='' /> */}
+            {/* <img src='https://images.craftsnherbs.com/beedev/flyingbee.gif' alt='gif' id='overlay' /> */}
+            {/* <map name="image-map">
                 <area alt="services" href="/Services" coords="216,35,172,107,216,185,307,185,350,111,303,34" shape="poly" className='services' />
                 <area alt="develop" href="#develop" coords="356,115,314,189,357,264,443,267,489,189,446,116" shape="poly" className='webdev' />
                 <area alt="maintain" href="#maintain" coords="496,34,453,107,497,184,584,185,628,110,585,34" shape="poly" className='webdev' />
                 <area alt="repair" href="#repair" coords="634,115,592,190,636,265,725,267,766,190,724,115" shape="poly" className='webdev' />
-            </map>
+            </map> */}
             <img src='https://images.craftsnherbs.com/beedev/beeline.png' alt='beeline' id='beeline' />
             <div className='questions'>
                 <div className='question-head'>
