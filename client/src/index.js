6794eedd774510c6d6a850dbf3bc32f892aa138c
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-12-21 11:44:13 -0500

    merging all changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { store } from './store'
 import App from './App'
 
-render(<Router><App /></Router>, document.querySelector('#root'))
+render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, 
+    document.querySelector('#root'))
